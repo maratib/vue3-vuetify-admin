07236b36b80fb16d013098635f79158a468208fd
Maratib Ali Khan <maratib@gmail.com>
2023-02-26 02:01:23 +0500

    initial commit

---
+++ src/layouts/admin/AdminLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <admin-bar />
+
+    <admin-view />
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import AdminBar from './AdminBar.vue';
+import AdminView from './AdminView.vue';
+</script>
